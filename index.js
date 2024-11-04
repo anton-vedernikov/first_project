0c86836bda2b8903dc5e54ac71aa9c0d73ea1507
Anton Vedernikov <ahtoh.vedernikov@gmail.com>
2024-11-04 18:22:40 +0700

    new console.log

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-const fs = require("fs");
-const answer = '1111111111';
-fs.writeFile("output.txt", answer, (err) => {});
+//const fs = require("fs");
+console.log(['5', ['10', '-4', '5', '0', '2'].join(' ')].join('\n'))
+//const answer = '1111111111';
+//fs.writeFile("output.txt", answer, (err) => {});
